69446b0b37e38c453f69814f6d808806b4fe2026
BlackJoker9 <75088450+BlackJoker9@users.noreply.github.com>
2021-06-06 01:44:38 +0530

    Update RDP_Coffin.ps1

--- RDP_Coffin.ps1
+++ RDP_Coffin.ps1
@@ -6,6 +6,5 @@ Copy ngrok.exe C:\Windows\System32
 Start-Service -Name audiosrv
 cmd /c echo ./ngrok.exe authtoken "$NGROK" >a.ps1
 cmd /c echo cmd /k start ngrok.exe tcp 3389 >>a.ps1
-cmd /c echo ping -n 999999 10.10.10.10 >>a.ps1
  Write-Host "Visit: https://dashboard.ngrok.com/endpoints/status to get NGROK IP"
 .\a.ps1
